0d0d08c27f5d4f5a8ad2dd9fbb648148bf0a10c6
Jhonata <jhomcosta@gmail.com>
2021-04-05 21:45:15 -0300

    Ignite-chapter-02

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
+import { useState } from 'react';
+import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
+import Modal from 'react-modal';
 import { GlobalStyle } from './styles/global'
+import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsProvider } from './hooks/useTransactions';
 
+
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+  
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+  
   return (
-    <>
-      <Header />
+    <TransactionsProvider>
+      <Header  onOpenNewTransactionModal = { handleOpenNewTransactionModal }/>
+
+      <Dashboard />
+
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen} 
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
